a33de9d983200ca713b0eb0fc59745a7df2c9b8e
dcj64 <aqsp01@gmail.com>
2022-07-17 14:58:11 +0100

    Part 2 completed Procedurally generated dungeon created

--- tile_types.py
+++ tile_types.py
@@ -32,8 +32,8 @@ def new_tile(
 
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True, transparent=True, dark=(ord("."), (255, 255, 255), (95, 130, 145)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False, transparent=False, dark=(ord("#"), (15, 5, 5), (255, 255, 255)),
 )
