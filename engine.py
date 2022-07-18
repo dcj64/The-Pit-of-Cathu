3efc4c0d2ea1f9a2dbc9a17a0a25da3e048213c3
dcj64 <aqsp01@gmail.com>
2022-07-18 17:01:13 +0100

    Part 5 completed Able to populate dungeon with enemies

--- engine.py
+++ engine.py
@@ -1,5 +1,3 @@
-from typing import Set, Iterable, Any
-
 from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
@@ -7,11 +5,11 @@ from tcod.map import compute_fov
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
+from typing import Iterable, Any
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
@@ -40,11 +38,6 @@ class Engine:
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-
-        for entity in self.entities:
-            # Only print entities that are in the FOV
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
